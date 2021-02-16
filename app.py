9e69575f97f2900d3d36089a2bffe33e1137d0c7
Bob Wei <bob.p.wei@gmail.com>
2021-02-15 21:21:18 -0500

    Added authentication via authorization tokens
    * Added the use and validation of auth tokens to app.py
    * Added test cases to test for auth tokens in tests.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, Response, request
 from flask_restful import Api, Resource
 from werkzeug.exceptions import HTTPException
 
+AUTH_TOKENS = ['W8cwfEUN2BsBNt5G4rMGQu9A',
+               'n9hnzJY3MKjU5JXSSrH5hXfR',
+               'suqku8Qm4FqUynH98cL98ZJG',
+               'M7SnndsM7tVs7hfBhhJbADgv',
+               'rzFsMgKEYsNRvdZnhBHSw8JC']
+
 class HelloAPI(Resource):
     def get(self):
         data = {'Hello': 'Welcome to the Echo Room.'}
@@ -12,10 +18,19 @@ class HelloAPI(Resource):
 
 class EchoAPI(Resource):
 
+    AUTH_KEY = 'auth'
     STR_KEY = 'str'
 
     def post(self):
         data = request.get_json()
+        try:
+            a = data[self.AUTH_KEY]
+        except (TypeError, KeyError):
+            raise NotAuthorizedError
+
+        if not a in AUTH_TOKENS:
+            raise NotAuthorizedError
+
         try:
             s = data[self.STR_KEY]
         except (TypeError, KeyError):
@@ -31,6 +46,9 @@ class InternalServerError(HTTPException):
 class NoStringProvidedError(HTTPException):
     pass
 
+class NotAuthorizedError(HTTPException):
+    pass
+
 errors = {
     'InternalServerError': {
         'message': 'Something went wrong',
@@ -39,6 +57,10 @@ errors = {
         'message': ('Request must contain string to be echoed in variable ' 
                     '\'str\''),
         'status': 400},
+    'NotAuthorizedError': {
+        'message': ('Request must contain valid authorization token in '
+                    'valiable \'auth\''),
+        'status': 401},
     }
 
 app = Flask(__name__)
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import json
+import random
 import unittest
-from app import app, errors
+from app import app, errors, AUTH_TOKENS
 
 class ServerLiveTest(unittest.TestCase):
     def setUp(self):
@@ -12,28 +13,37 @@ class ServerLiveTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 class EchoApiTest(unittest.TestCase):
-
+    
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
+        self.auth_token = random.choice(AUTH_TOKENS)
 
     def test_echo_different_strings(self):
+        payload = {'auth': self.auth_token}
         strs = ['foobar', '123456', 'amy_bob$']
         for s in strs:
+            payload['str'] = s
             response = self.app.post('/echo', 
-                                     data=json.dumps({'str': s}),
+                                     data=json.dumps(payload),
                                      content_type='application/json')
-            self.assertEqual(response.get_json(), {'str': s})
+            response = response.get_json()
+            self.assertEqual(response, {'str': s})
 
     def test_echo_no_string(self):
-        response = self.app.post('/echo').get_json()
+        payload = {'auth': self.auth_token}
+        response = self.app.post('/echo',
+                                 data=json.dumps(payload),
+                                 content_type='application/json')
+        response = response.get_json()
         self.assertEqual(response, errors['NoStringProvidedError'])
 
     def test_echo_incorrect_key(self):
         keys = ['s', 'abc', '!@#', 'something else']
         for k in keys:
+            payload = {'auth': self.auth_token, k: 'foobar'}
             response = self.app.post('/echo',
-                                     data=json.dumps({k: 'foobar'}),
+                                     data=json.dumps(payload),
                                      content_type='application/json')
             response = response.get_json()
             self.assertEqual(response,
@@ -45,6 +55,7 @@ class EchoApiTest(unittest.TestCase):
                    'c': 'three',
                    'str': 'foobar',
                    'd': 'four',
+                   'auth': self.auth_token,
                    }
         response = self.app.post('/echo',
                                  data=json.dumps(payload),
@@ -53,6 +64,22 @@ class EchoApiTest(unittest.TestCase):
         self.assertFalse('a' in response)
         self.assertTrue('str' in response)
         self.assertEqual(response['str'], 'foobar')
+
+    def test_echo_no_auth_token(self):
+        payload = {'str': 'foobar'}
+        response = self.app.post('/echo',
+                                 data=json.dumps(payload),
+                                 content_type='application/json')
+        response = response.get_json()
+        self.assertEqual(response, errors['NotAuthorizedError'])
+
+    def test_echo_invalid_auth_token(self):
+        payload = {'auth': 'baz', 'str': 'foobar'}
+        response = self.app.post('/echo',
+                                 data=json.dumps(payload),
+                                 content_type='application/json')
+        response = response.get_json()
+        self.assertEqual(response, errors['NotAuthorizedError'])
 
 if __name__ == '__main__':
     unittest.main()
